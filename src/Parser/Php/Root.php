c5fc7550a36b47ff065b031be751a914dbba40cf
thehawk <thehawk@hoa-project.net>
2014-09-24 16:55:53 +0200

    use stack

---
+++ src/Parser/Php/Root.php
@@ -0,0 +1,12 @@
+<?php
+namespace Sohapi\Parser\Php {
+    class Root extends Generic {
+
+        public function visit($element, &$handle = null, $eldnah = null) {
+            //TODO : Use stack to be sure to read in the order
+
+        }
+
+
+    }
+}
